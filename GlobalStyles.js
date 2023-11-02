2e6d83e4e47505954a663768933ae8db8cea9085
hekuranc <hekuranc>
2023-11-02 08:01:58 +0000

    Pushed from Locofy Builder

--- GlobalStyles.js
+++ GlobalStyles.js
@@ -1,9 +1,9 @@
 /* fonts */
 export const FontFamily = {
-  interSemiBold: "Inter-SemiBold",
-  bodyMediumMedium: "Inter-Medium",
   bodyXsmallRegular: "Inter-Regular",
+  bodyMediumMedium: "Inter-Medium",
   bodyLargeBold: "Inter-Bold",
+  interSemiBold: "Inter-SemiBold",
 };
 /* font sizes */
 export const FontSize = {
@@ -15,28 +15,28 @@ export const FontSize = {
 };
 /* Colors */
 export const Color = {
-  passiveMenu: "#54575b",
-  activeMenu2: "#fe0010",
-  primary500: "#6be48b",
-  secondary500: "#292d32",
-  colorGray: "rgba(3, 3, 3, 0.8)",
   secondary200: "#9d9ea1",
+  activeMenu2: "#fe0010",
+  secondary500: "#292d32",
   secondary100: "#bdbebf",
   live: "#d05c5c",
   secondaryWhite: "#fff",
+  passiveMenu: "#54575b",
+  primary500: "#6be48b",
+  colorGray: "rgba(3, 3, 3, 0.8)",
 };
 /* Paddings */
 export const Padding = {
+  p_xs: 12,
+  p_5xs: 8,
   p_base: 16,
   p_xl: 20,
-  p_xs: 12,
   p_5xl: 24,
-  p_5xs: 8,
 };
 /* border radiuses */
 export const Border = {
-  br_101xl: 120,
-  br_6xs: 7,
   br_3xs: 10,
   br_smi_6: 13,
+  br_101xl: 120,
+  br_6xs: 7,
 };
